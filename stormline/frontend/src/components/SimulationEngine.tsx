9bf8e9e2a1e009560e8e5080874cd0ad5d69ae3f
Anvita Kallam <kranvita007@gmail.com>
2026-02-21 17:29:19 -0500

    Fix JSX structure: remove extra closing div
    - Removed extra closing div tag in comparison section
    - Fixed adjacent JSX elements error

--- stormline/frontend/src/components/SimulationEngine.tsx
+++ stormline/frontend/src/components/SimulationEngine.tsx
@@ -1180,7 +1180,6 @@ export default function SimulationEngine({ onStartSimulation }: SimulationEngine
             </button>
             </div>
           </div>
-          </div>
         </div>
       )}
     </>
