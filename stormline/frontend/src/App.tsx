84baa0ed349e5594a184ddc61d64f8521f6cd160
Anvita Kallam <kranvita007@gmail.com>
2026-02-21 16:58:15 -0500

    Fix: Add useCallback to React imports
    - useCallback was used but not imported
    - Fixes ReferenceError: useCallback is not defined

--- stormline/frontend/src/App.tsx
+++ stormline/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import Globe from './components/Globe'
 import CoverageChoropleth from './components/CoverageChoropleth'
 import SimulationEngine from './components/SimulationEngine'
